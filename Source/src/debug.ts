6af6d7b537f75bd55d23b842f676702626f1d4ed
jamesnewton5 <jamesnewton5@icloud.com>
2026-06-09 17:01:44 +1000

    .

--- Source/src/debug.ts
+++ Source/src/debug.ts
@@ -3,21 +3,21 @@ import { PACK_NAME } from "./data";
 
 // MARK: Config
 // ----------------------------------------------------------------
-export const debugSettingsRecords: Record<string, boolean> = {
-    main: true,
-    PlayerCache: true,
-    AddonMessage: true
+const DEBUG_SETTINGS: Record<string, boolean> = {
+    main: false,
+    PlayerCache: false,
+    AddonMessage: false
 };
 
 
 
 // MARK: Debug
 // ----------------------------------------------------------------
-if (debugSettingsRecords.main) console.warn(`\n§aStarting ${PACK_NAME} add-on`); // Creates a divider between the old console messages and the new ones for easier debugging
+if (DEBUG_SETTINGS.main) console.warn(`\n§aStarting ${PACK_NAME} add-on`); // Creates a divider between the old console messages and the new ones for easier debugging
 export class Debug {
     public static printDebug = function (this: any, message: string): void { // [i] First parameter "this" will be ignored by TS compiler
         const className = this.name;
-        if (!debugSettingsRecords[className]) return;
+        if (!DEBUG_SETTINGS[className]) return;
         console.info(`§r§7 ${system.currentTick.toString()}§f [${PACK_NAME}] ${className} - ${message}`);
     }
 }
